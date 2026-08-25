91eb038580a9fd7fc05b2b0c2e612b4f849392bb
bentraich25500 <bentraich25500@gmail.com>
2026-08-25 11:02:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const fs = require('fs');
 // Firebase Admin (for writing realtime stats to dashboards)
 let admin;
 try { admin = require('firebase-admin'); } catch {}
-const GuildConfigService = require('./config.json');
+const logger = require('./utils/logger');
 let devConfig = { developers: [] };
 try {
   devConfig = require('./config.json');
